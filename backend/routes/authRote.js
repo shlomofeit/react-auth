9b9a31b844c592b17d1f9be3bb71a1c3881a398b
shlomofeit <11.5sfeit@gmail.com>
2026-09-17 17:41:03 +0300

    fix the getProfile route to get

--- backend/routes/authRote.js
+++ backend/routes/authRote.js
@@ -11,7 +11,7 @@ const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", loginUser);
-router.post("/profile", authMiddleware, getProfile);
+router.get("/profile", authMiddleware, getProfile);
 router.post("/logout", logout);
 
 export default router;
