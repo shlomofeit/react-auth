996d6c563aef553a4bcd0255b73f2ee0e1d2d390
shlomofeit <11.5sfeit@gmail.com>
2026-09-17 17:31:29 +0300

    Added profile page content

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,5 +1,11 @@
-const profilePage = () => {
-  return <div>profilePage</div>;
+import { useAuth } from "../hooks/useAuth";
+import Profile from "../components/Profile";
+
+const ProfilePage = () => {
+  const { user } = useAuth();
+  if (!user) return null;
+
+  return <Profile user={user} />;
 };
 
-export default profilePage;
+export default ProfilePage;
