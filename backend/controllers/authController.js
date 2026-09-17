5bbe66d595fd1d4f68355a09ac6d0f3336ab0a28
shlomofeit <11.5sfeit@gmail.com>
2026-09-17 16:11:46 +0300

    Added getProfile controller function and change signup response to token

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,11 +2,9 @@ import { login, register } from "../services/authService.js";
 
 export const signup = async (req, res) => {
   const user = req.body;
-  const newId = await register(user);
+  const token = await register(user);
 
-  return res
-    .status(201)
-    .json({ msg: "User regitered successfully", id: newId });
+  return res.status(201).json({ token });
 };
 
 export const loginUser = async (req, res) => {
@@ -18,12 +16,15 @@ export const loginUser = async (req, res) => {
     secure: true,
     sameSite: "strict",
   });
-  return res
-    .status(200)
-    .json({ msg: `${user.username} logged in successfully` });
+  return res.status(200).json({ token });
 };
 
 export const logout = (req, res) => {
   res.clearCookie("token");
   return res.status(200).json({ message: "Logged out successfully" });
+};
+
+export const getProfile = (req, res) => {
+  const user = req.user;
+  return res.status(200).json({ user });
 };
