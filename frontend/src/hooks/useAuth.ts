befcd34f2c8a19b3a11a851b4a73b58b2a80624d
shlomofeit <11.5sfeit@gmail.com>
2026-09-17 17:11:15 +0300

    Added login and logout to useAuth hook

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -2,12 +2,18 @@ import axios from "axios";
 import type { SignupData } from "../types";
 import { useUserStore } from "../store/userUserStore";
 import { useState } from "react";
-import { getProfileRequest, signupRequest } from "../api/authApi";
+import {
+  getProfileRequest,
+  loginRequest,
+  logoutRequest,
+  signupRequest,
+} from "../api/authApi";
 
 export const useAuth = () => {
   const user = useUserStore((state) => state.user);
   const token = useUserStore((state) => state.token);
   const setToken = useUserStore((state) => state.setToken);
+  const clearToken = useUserStore((state) => state.clearToken);
 
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
@@ -32,5 +38,35 @@ export const useAuth = () => {
     }
   };
 
-  return { user, token, loading, error, signup };
+  const login = async (email: string, password: string) => {
+    setLoading(true);
+    setError(null);
+    try {
+      const { token } = await loginRequest(email, password);
+      const { user } = await getProfileRequest(token);
+      setToken(user, token);
+      return true;
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        if (err.code === "401") {
+          setError("uauthorization");
+        }
+      } else {
+        setError("server error");
+      }
+      return false;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      await logoutRequest();
+    } finally {
+      clearToken();
+    }
+  };
+
+  return { user, token, loading, error, login, signup, logout };
 };
