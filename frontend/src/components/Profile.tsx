6e61082f6553d23a16858e6486ac41c1760f151c
shlomofeit <11.5sfeit@gmail.com>
2026-09-17 17:29:34 +0300

    Added profile component content

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -1,5 +1,16 @@
-const profile = () => {
-  return <div>profile</div>;
+import type { User } from "../types";
+
+const Profile = (user: User) => {
+  return (
+    <div>
+      <h2>Profile</h2>
+      <p>Username: {user.username}</p>
+      <br />
+      <p>Email: {user.email}</p>
+      <br />
+      <p>Role: {user.role}</p>
+    </div>
+  );
 };
 
-export default profile;
+export default Profile;
