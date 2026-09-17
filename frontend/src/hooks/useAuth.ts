3d2041b238c18a2fb84e1db1d7855a69cc109766
shlomofeit <11.5sfeit@gmail.com>
2026-09-17 17:05:00 +0300

    Added loading and error states to useAuth hook

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -1,5 +1,7 @@
+import axios from "axios";
 import type { SignupData } from "../types";
 import { useUserStore } from "../store/userUserStore";
+import { useState } from "react";
 import { getProfileRequest, signupRequest } from "../api/authApi";
 
 export const useAuth = () => {
@@ -7,12 +9,28 @@ export const useAuth = () => {
   const token = useUserStore((state) => state.token);
   const setToken = useUserStore((state) => state.setToken);
 
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
   const signup = async (data: SignupData) => {
-    const { token } = await signupRequest(data);
-    const { user } = await getProfileRequest(token);
-    setToken(user, token);
-    return true;
+    setLoading(true);
+    setError(null);
+    try {
+      const { token } = await signupRequest(data);
+      const { user } = await getProfileRequest(token);
+      setToken(user, token);
+      return true;
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        if (err.code === "401") setError("uauthorization");
+      } else {
+        setError("server error");
+      }
+      return false;
+    } finally {
+      setLoading(false);
+    }
   };
 
-  return { user, token, signup };
+  return { user, token, loading, error, signup };
 };
